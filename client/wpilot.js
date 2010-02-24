476e9841d3deeac3e1110858da3945e66a576423
Johan Dahlberg <dahlberg.johan@gmail.com>
2010-02-25 00:37:40 +0100

    tweaked small hud message font

--- client/wpilot.js
+++ client/wpilot.js
@@ -12,7 +12,7 @@ var GRID_CELL_SIZE      = 250;
     GRID_CELL_COLOR     = 'rgba(255,255,255,0.2)';
     
 // GUI Fonts used in the client.
-var HUD_SMALL_FONT      = 'bold 9px Arial',
+var HUD_SMALL_FONT      = 'bold 10px Arial',
     HUD_LARGE_FONT      = 'bold 11px Arial',
     HUD_XLARGE_FONT      = 'bold 16px Arial',
     HUD_WHITE_COLOR     = 'rgba(255,255,255,0.8)',
