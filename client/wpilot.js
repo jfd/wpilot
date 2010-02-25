6111bf053bf0aca6ba4be2de496fd5027474595a
Johan Dahlberg <dahlberg.johan@gmail.com>
2010-02-25 10:35:10 +0100

    Version 0.8

--- client/wpilot.js
+++ client/wpilot.js
@@ -6,7 +6,7 @@
 //  
 //  Copyright (c) 2010 Johan Dahlberg 
 //
-var CLIENT_VERSION = '(develop version)';
+var CLIENT_VERSION = '0.8';
 
 var GRID_CELL_SIZE      = 250;
     GRID_CELL_COLOR     = 'rgba(255,255,255,0.2)';
