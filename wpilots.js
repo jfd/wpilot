2b61a1b6312104c9562fcdd1559884344f22e594
Sven Neuhaus <sven@sven.de>
2012-10-13 23:58:01 +0200

    Crash fix for race condition

--- wpilots.js
+++ wpilots.js
@@ -88,8 +88,8 @@ const SWITCHES = [
 
 // Default server options
 const DEFAULT_OPTIONS = {
-  debug:                true,
-  name:                 'WPilot Server',
+  debug:                false,
+  name:                 'WPilot server',
   host:                 '127.0.0.1',
   region:               'n/a',
   admin_password:       null,
@@ -341,7 +341,13 @@ function start_gameserver(maps, options, shared) {
 
       if (connection.last_ping + 2000 < time) {
         connection.last_ping = time;
-        connection.send(JSON.stringify([PING_PACKET]));
+        try {
+          connection.send(JSON.stringify([PING_PACKET]));
+        } catch (err) {
+          // most likely the socket is not opened
+          connection.set_state(DISCONNECTED);
+          continue;
+        }
       }
       if (update_tick % connection.update_rate != 0) {
         continue;
@@ -868,7 +874,7 @@ function start_gameserver(maps, options, shared) {
       }
     });
 
-    // Connection 'close' event listener. Occurs when the connection is 
+    // Connection 'close' event listener. Occures when the connection is
     // closed by user or server.
     conn.on('close', function() {
       conn.set_state(DISCONNECTED);
